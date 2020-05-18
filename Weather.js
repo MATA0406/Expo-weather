4c167acffe9e3567de6c65729ba894a9781f4a7e
JungJinHo <mata@ryencatchers.com>
2020-05-18 11:35:49 +0900

    Complate

--- Weather.js
+++ Weather.js
@@ -6,24 +6,34 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const weatherOptions = {
   Haze: {
-    iconName: "weather-lightning-rainy",
+    iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "Haze",
+    subtitle: "Just don't go outside",
   },
   Thunderstorm: {
-    iconName: "",
-    gradient: ["", ""],
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"],
+    title: "Thunderstorm in the house",
+    subtitle: "Actually, outside of the house",
   },
   Drizzle: {
     iconName: "weather-hail",
     gradient: ["#89F7FE", "#66A6FF"],
+    title: "Drizzle",
+    subtitle: "Is like rain",
   },
   Rain: {
     iconName: "weather-rainy",
     gradient: ["#00C6FB", "#005BEA"],
+    title: "Raining like a MF",
+    subtitle: "For more info look outside",
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "cold as balls",
+    subtitle: "Do you want to build a snowman?",
   },
   Atmosphere: {
     iconName: "weather-hail",
@@ -32,18 +42,26 @@ const weatherOptions = {
   Clear: {
     iconName: "weather-sunny",
     gradient: ["#FEF253", "#FF7300"],
+    title: "Sunny",
+    subtitle: "So Hot",
   },
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#D7D2CC", "#304352"],
+    title: "Clouds",
+    subtitle: "I know, funcking boring ",
   },
   Mist: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "Mist",
+    subtitle: "'It's like you have no glasses on.",
   },
   Dust: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "Dusty",
+    subtitle: "Thanks a lot China",
   },
 };
 
@@ -63,8 +81,10 @@ export default function Weather({ temp, condition }) {
         <Text style={styles.temp}>{temp}°</Text>
       </View>
       <View style={{ ...styles.harfContainer, ...styles.textContainer }}>
-        <Text style={styles.title}>In case this is too long</Text>
-        <Text style={styles.subtitle}>and this one as well then?</Text>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
       </View>
     </LinearGradient>
   );
